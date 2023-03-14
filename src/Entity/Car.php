8a3213ff9ee717c45f184e84111147f8a7a2481d
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-03-14 16:32:24 +0100

    Create car entity

---
+++ src/Entity/Car.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $marque = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $modele = null;
+
+    #[ORM\Column]
+    private ?int $kilometrage = null;
+
+    #[ORM\Column]
+    private ?int $annee = null;
+
+    #[ORM\Column]
+    private ?int $nombrePortes = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photofilename = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getKilometrage(): ?int
+    {
+        return $this->kilometrage;
+    }
+
+    public function setKilometrage(int $kilometrage): self
+    {
+        $this->kilometrage = $kilometrage;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?int
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(int $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function getNombrePortes(): ?int
+    {
+        return $this->nombrePortes;
+    }
+
+    public function setNombrePortes(int $nombrePortes): self
+    {
+        $this->nombrePortes = $nombrePortes;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getPhotofilename(): ?string
+    {
+        return $this->photofilename;
+    }
+
+    public function setPhotofilename(string $photofilename): self
+    {
+        $this->photofilename = $photofilename;
+
+        return $this;
+    }
+}
